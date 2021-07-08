8756850d427bb7b67aea783ee2c77cbb15218441
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2021-07-08 02:52:25 -0500

    adjust responsive header and add assets

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
+    @ViewChild('hamburguerMenu') hamburguerMenu: ElementRef;
+    @ViewChild('menuIcon') menuIcon: ElementRef;
+    @ViewChild('closeIcon') closeIcon: ElementRef;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor(private renderer2: Renderer2) { }
+
+    ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        this.openMenu();
+        this.closeMenu();
+    }
+
+    openMenu(): void {
+        const asHamburguerMenu = this.hamburguerMenu.nativeElement;
+        const asMenuIcon = this.menuIcon.nativeElement;
+        this.renderer2.listen(asMenuIcon, 'click', (evt) => {
+            this.renderer2.addClass(document.body, 'noscroll');
+            this.renderer2.addClass(asHamburguerMenu, 'show');
+        });
+    }
+
+    closeMenu(): void {
+        const asHamburguerMenu = this.hamburguerMenu.nativeElement;
+        const asCloseIcon = this.closeIcon.nativeElement;
+        this.renderer2.listen(asCloseIcon, 'click', (evt) => {
+            this.renderer2.removeClass(document.body, 'noscroll');
+            this.renderer2.removeClass(asHamburguerMenu, 'show');
+        });
+    }
 
 }
